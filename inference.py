597df0df59e362c2f93cea3bdb21d448070be29b
hoanglv <hoanglv282004@gmail.com>
2025-10-06 21:35:29 +0700

    inference

---
+++ inference.py
@@ -0,0 +1,53 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from utils import *
+import argparse
+import os 
+from transformers.integrations import WandbCallback
+os.environ["WANDB_DISABLED"] = "true"
+
+seed_everything(22520465) 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str)
+    parser.add_argument("--claim_model", type=str2bool)
+    parser.add_argument("--lang", type=str)
+    parser.add_argument("--max_length", type=int)
+    parser.add_argument("--use_prompt", type=str)
+    parser.add_argument("--segment", type=str2bool)
+    parser.add_argument("--intrinsic", type=int)
+    parser.add_argument("--extrinsic", type=int)
+    parser.add_argument("--no", type=int)
+    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--fold", type=int)
+    args = parser.parse_args()
+    config = vars(args)
+
+    if config["claim_model"]:
+        from semviqa.tvc.model import ClaimModelForClassification
+        model = ClaimModelForClassification.from_pretrained(config["model_path"])
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(config["model_path"])
+    tokenizer = AutoTokenizer.from_pretrained(config["model_name"])
+    trainer = Trainer(model=model, tokenizer=tokenizer)
+    trainer.remove_callback(WandbCallback)
+
+    train_dataset, pubtest_dataset, privatetest_dataset, tokenizer, id2label = preparing_dataset("data/train_dsc.csv", "data/public_test.csv", "data/private_test.csv", config["segment"], config["intrinsic"], config["extrinsic"], config["no"], config["model_name"])
+    encoded_privatetest_dataset = privatetest_dataset.map(
+        preprocess_and_tokenize,
+        batched=True,
+        fn_kwargs={"max_length": config["max_length"], "use_prompt": config["use_prompt"], "tokenizer": tokenizer, "lang": config["lang"]}
+    )
+    safe_model_name = config['model_name'].replace("/", "_")
+    output_dir = config[f"output/{safe_model_name}/fold_{config['fold']}"]
+    os.makedirs(output_dir, exist_ok=True)
+    evaluate_pritest(trainer,encoded_privatetest_dataset, id2label, output_dir)
---
+++ inference.sh
@@ -0,0 +1,114 @@
+pip install -r requirements.txt
+
+# Gán biến điều khiển (true hoặc false), giữ nguyên nếu đã có biến môi trường TRANSLATE
+TRANSLATE=${TRANSLATE:-true}
+
+# Kiểm tra điều kiện trước khi chạy
+if [ "$TRANSLATE" = true ]; then
+    echo "=== Đang dịch dữ liệu ==="
+    python translate_data.py
+else
+    echo "=== Bỏ qua bước dịch dữ liệu ==="
+fi
+
+# train deberta
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "microsoft/deberta-xlarge-mnli" \
+        --model_path "results/model_microsoft_deberta-xlarge-mnli/fold_${fold}" \
+        --max_length 512 \
+        --use_prompt "no" \
+        --claim_model False \
+        --segment False \
+        --intrinsic 0 \
+        --extrinsic 1 \
+        --no 2 \
+        --lang "en" \
+        --fold ${fold}
+done
+
+# train cross 
+pip install transformers==4.52.4
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "cross-encoder/nli-deberta-v3-large" \
+        --model_path "results/model_cross-encoder_nli-deberta-v3-large/fold_${fold}" \
+        --max_length 512 \
+        --use_prompt 'no' \
+        --claim_model False \
+        --segment False \
+        --intrinsic 0 --extrinsic 2 --no 1 \
+        --lang "en" --fold ${fold}
+done
+
+
+# train dvt 
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "dangvantuan/vietnamese-document-embedding" \
+        --model_path "results/model_dangvantuan_vietnamese-document-embedding/fold_${fold}" \
+    --max_length 512 \
+    --use_prompt 'no' \
+    --claim_model False \
+    --segment False \
+    --intrinsic 0 --extrinsic 1 --no 2 \
+    --lang "vi" --fold ${fold}
+done
+
+# train roberta 
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "FacebookAI/roberta-large-mnli" \
+        --model_path "results/model_FacebookAI_roberta-large-mnli/fold_${fold}" \
+    --max_length 512 \
+    --use_prompt 'no' \
+    --claim_model False \
+    --segment False \
+    --intrinsic 0 --extrinsic 1 --no 2 \
+    --lang "en" --fold ${fold}
+done
+
+
+pip install --upgrade transformers==4.42.3 peft==0.11.1 datasets==2.20.0 accelerate==0.32.1
+pip install semviqa --no-deps
+
+# train erniem 
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "SemViQA/tc-erniem-viwikifc" \
+        --model_path "results/model_SemViQA_tc-erniem-viwikifc/fold_${fold}" \
+    --max_length 512 \
+    --use_prompt 'no' \
+    --claim_model True \
+    --segment False \
+    --intrinsic 2 --extrinsic 0 --no 1 \
+    --lang "vi" --fold ${fold}
+done
+
+# train xlm-roberta 
+for fold in {0..5}
+do
+    echo "Running inference for fold $fold..."
+    python inference.py \
+        --model_name "SemViQA/tc-xlmr-isedsc01" \
+        --model_path "results/model_SemViQA_tc-xlmr-isedsc01/fold_${fold}" \
+    --max_length 512 \
+    --use_prompt 'no' \
+    --claim_model True \
+    --segment False \
+    --intrinsic 2 --extrinsic 0 --no 1 \
+    --lang "vi" --fold ${fold}
+done
+
+# stack ensemble
+python stack_ensemble.py
