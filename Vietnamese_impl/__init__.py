a2a1dd13d51745f07c3a838e8067f93ad486606c
hoanglv <hoanglv282004@gmail.com>
2025-10-09 21:04:21 +0700

    done

---
+++ Vietnamese_impl/__init__.py
@@ -0,0 +1,20 @@
+from .configuration import VietnameseConfig
+from .modeling import (
+    VietnameseModel,
+    VietnameseForMaskedLM,
+    VietnameseForMultipleChoice,
+    VietnameseForQuestionAnswering,
+    VietnameseForSequenceClassification,
+    VietnameseForTokenClassification,
+)
+
+__all__ = [
+    "VietnameseConfig",
+    "VietnameseModel",
+    "VietnameseForMaskedLM",
+    "VietnameseForMultipleChoice",
+    "VietnameseForQuestionAnswering",
+    "VietnameseForSequenceClassification",
+    "VietnameseForTokenClassification",
+]
+
